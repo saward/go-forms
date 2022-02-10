f0e7d30f6473cef7b90df25be91a408e343c8f66
saward <mark.saward@episub.com>
2022-02-10 19:23:19 +1100

    refactor names and simplify addition of errors

--- validate/validation.go
+++ validate/validation.go
@@ -19,141 +19,113 @@ type NumericComparable interface {
 
 // Validate records the provided error, if not nil, inside the errors list
 // marked against the provided field.
-func Validate(field string, errors Errors, err error) {
-	if err != nil {
-		errors[field] = append(errors[field], err.Error())
-	}
+func AddError(field string, errors Errors, msg string) {
+	errors[field] = append(errors[field], msg)
 }
 
 // StringLength Checks that a string has either an exact count of characters,
 // or fits within the specified range of m to n (inclusive).
-func StringLength(
+func IsStringLength(
 	field string,
 	errors Errors,
 	v string,
 	m int,
 	n int,
 ) {
-	Validate(field, errors, func(v string, m int, n int) error {
-		var msg string
-		if m == n {
-			msg = fmt.Sprintf("Must be exactly %d characters long", m)
-		} else {
-			msg = fmt.Sprintf("Must be between %d and %d characters long", m, n)
-		}
+	var msg string
+	if m == n {
+		msg = fmt.Sprintf("Must be exactly %d characters long", m)
+	} else {
+		msg = fmt.Sprintf("Must be between %d and %d characters long", m, n)
+	}
 
-		if len(v) < m || len(v) > n {
-			return fmt.Errorf(msg)
-		}
-
-		return nil
-	}(v, m, n))
+	if len(v) < m || len(v) > n {
+		AddError(field, errors, msg)
+	}
 }
 
 // NumberBetween Checks that the integer typed variable is exactly m == n in
 // size, or between m and n inclusive.
-func NumberBetween[T NumericComparable](
+func IsNumberBetween[T NumericComparable](
 	field string,
 	errors Errors,
 	v T,
 	m T,
 	n T,
 ) {
-	Validate(field, errors, func(v T, m T, n T) error {
-		var msg string
+	var msg string
 
-		if m == n {
-			msg = fmt.Sprintf("Must be exactly %d, but was %d", m, v)
-		} else {
-			msg = fmt.Sprintf("Must be between %d and %d, but was %d", m, n, v)
-		}
+	if m == n {
+		msg = fmt.Sprintf("Must be exactly %d, but was %d", m, v)
+	} else {
+		msg = fmt.Sprintf("Must be between %d and %d, but was %d", m, n, v)
+	}
 
-		if v < m || v > n {
-			return fmt.Errorf(msg)
-		}
-
-		return nil
-	}(v, m, n))
+	if v < m || v > n {
+		AddError(field, errors, msg)
+	}
 }
 
 // Size checks that an array or map has either exactly m == n entries, or
 // between m and n entries (inclusive)
-func Size[T Lengthable[Q, U], Q any, U comparable](
+func IsSize[T Lengthable[Q, U], Q any, U comparable](
 	field string,
 	errors Errors,
 	v T,
 	m int,
 	n int,
 ) {
-	Validate(field, errors, func(v T, m int, n int) error {
-		var msg string
-		if m == n {
-			msg = fmt.Sprintf("Must have exactly %d entries, but had %d", m, len(v))
-		} else {
-			msg = fmt.Sprintf("Must have between %d and %d entries, but had %d", m, n, len(v))
-		}
+	var msg string
+	if m == n {
+		msg = fmt.Sprintf("Must have exactly %d entries, but had %d", m, len(v))
+	} else {
+		msg = fmt.Sprintf("Must have between %d and %d entries, but had %d", m, n, len(v))
+	}
 
-		if len(v) < m || len(v) > n {
-			return fmt.Errorf(msg)
-		}
-
-		return nil
-	}(v, m, n))
+	if len(v) < m || len(v) > n {
+		AddError(field, errors, msg)
+	}
 }
 
 // MinSize checks that an array or map has at least n entries
-func MinSize[T Lengthable[Q, U], Q any, U comparable](
+func IsMinSize[T Lengthable[Q, U], Q any, U comparable](
 	field string,
 	errors Errors,
 	v T,
 	n int,
 ) {
-	Validate(field, errors, func(v T, n int) error {
-		entry := "entry"
-		if n > 1 {
-			entry = "entries"
-		}
+	entry := "entry"
+	if n > 1 {
+		entry = "entries"
+	}
 
-		var msg string
-		msg = fmt.Sprintf("Must have a minimum of %d %s, but had %d", n, entry, len(v))
+	var msg string
+	msg = fmt.Sprintf("Must have a minimum of %d %s, but had %d", n, entry, len(v))
 
-		if len(v) < n {
-			return fmt.Errorf(msg)
-		}
-
-		return nil
-	}(v, n))
+	if len(v) < n {
+		AddError(field, errors, msg)
+	}
 }
 
 // Regex Confirms that value matches the provided regex
-func Regex(
+func IsRegex(
 	field string,
 	errors Errors,
 	v string,
 	rx *regexp.Regexp,
 	message string,
 ) {
-	Validate(field, errors, func(rx *regexp.Regexp, value string, message string) error {
-		if !rx.MatchString(value) {
-			return fmt.Errorf(message)
-		}
-
-		return nil
-	}(rx, v, message))
+	if !rx.MatchString(v) {
+		AddError(field, errors, message)
+	}
 }
 
 // Email Confirms that value matches our provided email regex.  For a custom
 // email regex, use Regex.
-func Email(
+func IsEmail(
 	field string,
 	errors Errors,
 	v string,
 ) {
-	Validate(field, errors, func(rx *regexp.Regexp, value string, message string) error {
-		if !rx.MatchString(value) {
-			return fmt.Errorf(message)
-		}
-
-		return nil
-	}(EmailRx, v, "Email address is invalid"))
+	IsRegex(field, errors, v, EmailRx, "Email address is invalid")
 }
